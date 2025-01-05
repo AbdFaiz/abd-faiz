45a72f81233174c7715537d02e4e766142ac4151
AbdFaiz <abdurrahmanfaiz187@gmail.com>
2025-01-05 16:43:35 +0700

    up to repo

---
+++ script.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function() {
+    gsap.from(".navbar", { opacity: 0, y: -50, duration: 1 });
+    gsap.from(".hero-text", { opacity: 0, y: 50, duration: 1, delay: 0.5 });
+    gsap.from(".hero-image", {
+        scale: 0.5,
+        opacity: 0,
+        duration: 1.5,
+        delay: 0.8,
+    });
+    gsap.from(".scroll-down", {
+        opacity: 0,
+        y: 20,
+        repeat: -1,
+        yoyo: true,
+        duration: 1,
+    });
+
+    AOS.init({
+        duration: 1000,
+        easing: "ease-in-out",
+    });
+
+    var options = {
+        strings: ["Software Enginer Student", "Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 1000,
+        startDelay: 500,
+        loop: true,
+    };
+
+    var typed = new Typed(".typewriter-text", options);
+
+});
+
+function sendEmail(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const need = document.getElementById('need').value;
+    const email = document.getElementById('email').value;
+
+    if (!name || !need || !email) {
+        alert("Please fill out all fields before submitting.");
+        return;
+    }
+
+    const subject = `Contact Form Submission from ${encodeURIComponent(name)}`;
+    const body = `Name: ${encodeURIComponent(name)}%0A` +
+        `Need: ${encodeURIComponent(need)}%0A` +
+        `Email: ${encodeURIComponent(email)}`;
+
+    const mailtoLink = `mailto:abdurrahmanfaiz187@gmail.com?subject=${encodeURIComponent(subject)}&body=${body}`;
+
+    // Open the mailto link
+    window.location.href = mailtoLink;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+        fontFamily: {
+            'serif': ['Playfair Display', 'Helvetica', 'Times New Roman'],
+
+            'sans': 'Helvetica, Arial, sans-serif',
+        }
+    }
+}
